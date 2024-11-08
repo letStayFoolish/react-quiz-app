95991eaa3c04c8f80d359dc3bfc928f11dffdfb8
stayFoolish <nemanjakaraklajic90@icloud.com>
2024-11-08 19:55:48 +0100

    feat: Add quiz completion screen and shuffle answers
    Implemented a quiz completion screen with a congratulatory image and message, and included a check to prevent further answers after the quiz is finished. Additionally, answers are now shuffled to ensure they appear in random order for each question.

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import QUESTIONS from "../questions.ts";
-
+import quizComplete from "../assets/quiz-complete.png";
 type Props = {};
 
 const Quiz: React.FC = () => {
@@ -8,16 +8,33 @@ const Quiz: React.FC = () => {
 
   const activeQuestionIndex = userAnswers.length;
 
+  const isQuizFinished = activeQuestionIndex === QUESTIONS.length;
+
   const handleSelectAnswer = (selectedAnswer: string) => {
+    if (isQuizFinished) return;
     setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
   };
+
+  if (isQuizFinished) {
+    return (
+      <div id="summary">
+        <img src={quizComplete} alt="Trophy icon" />
+        <h2>Quiz Complete</h2>
+      </div>
+    );
+  }
+
+  // .sort() will not create a new array, instead it will modify the original array
+  // That is why is needed to create new array to be used in combination with sort
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="qiestion">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li className="answer" key={answer}>
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
